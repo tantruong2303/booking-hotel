ab968930c725ae1d4eec2c463e59266df612673a
Cao Chi Hai <haicao2805@gmail.com>
2021-05-26 10:48:44 +0700

    refactor roomlist

--- src/java/controllers/AddReviewServlet.java
+++ src/java/controllers/AddReviewServlet.java
@@ -5,26 +5,21 @@
  */
 package controllers;
 
-import daos.RoomDAO;
-import dtos.Room;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import utils.Helper;
-import utils.Validator;
 
 /**
  *
  * @author HaiCao
  */
-@WebServlet(name = "RoomListController", urlPatterns = {"/RoomListController"})
-public class RoomListController extends HttpServlet {
+@WebServlet(name = "AddReviewServlet", urlPatterns = {"/AddReviewServlet"})
+public class AddReviewServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -40,26 +35,6 @@ public class RoomListController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         Helper.protectedRouter(request, response, 0, "login.jsp");
         
-        String listRoomPage = "listRoom.jsp";
-        RoomDAO roomDAO = new RoomDAO();
-        ArrayList<Room> list = new ArrayList<>();
-        
-        int numOfPeople = Validator.getIntParams(request, "numOfPeople", "numOfPeople", 1, 10, -1);
-        float min = Validator.getFloatParams(request, "min", "price", 1, Float.MAX_VALUE, -1);
-        float max = Validator.getFloatParams(request, "max", "price", 1, Float.MAX_VALUE, -1);
-        String priceOrder = Validator.getStringParam(request, "priceOrder", "price", 1, 4, "ASC");
-
-        if(numOfPeople > -1 && min > -1 && max > -1)
-            list = roomDAO.getRoomByNumOfPeopleAndPrice(numOfPeople, min, max, priceOrder);
-        else if(numOfPeople == -1 && min > -1 && max > -1)
-            list = roomDAO.getRoomByPrice(min, max, priceOrder);
-        else if(numOfPeople > -1 && min == -1 && max == -1)
-            list = roomDAO.getRoomByNumOfPeople(numOfPeople);
-        else list = roomDAO.getAllRoom();
-            
-        request.setAttribute("listRoom", list);
-        RequestDispatcher rd = request.getRequestDispatcher(listRoomPage);
-        rd.forward(request, response);
         
     }
 
---
+++ src/java/daos/ReviewDAO.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daos;
+
+import dtos.Review;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import utils.Connector;
+
+/**
+ *
+ * @author HaiCao
+ */
+public class ReviewDAO {
+    public boolean addReview(Review review){
+        try {
+            Connection connection = Connector.getConnection();
+            String sql = "INSERT INTO tbl_Review (message, rate, createDate, userId, roomId) VALUES (?,?,?,?,?)";
+            
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, review.getMessage());
+            pstmt.setInt(2, review.getRate());
+            pstmt.setDate(3, review.getCreateDate());
+            pstmt.setInt(4, review.getUserId());
+            pstmt.setInt(5, review.getRoomId());
+            
+            pstmt.executeUpdate();
+            connection.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
