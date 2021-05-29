614833329e494cbb3e7100938e6fe53ec847a233
Cao Chi Hai <haicao2805@gmail.com>
2021-05-29 09:44:34 +0700

    fix bug add review

--- src/java/controllers/CancelBookingInfo.java
+++ src/java/controllers/CancelBookingInfo.java
@@ -15,7 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import constant.Routers;
+import daos.ReviewDAO;
 import daos.RoomDAO;
+import daos.UserDAO;
 import dtos.BookingInfo;
 import utils.GetParam;
 import utils.Helper;
@@ -103,36 +105,6 @@ public class CancelBookingInfo extends HttpServlet {
             RequestDispatcher rd = request.getRequestDispatcher(Routers.ERROR);
             rd.forward(request, response);
         }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
-    // + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /**
