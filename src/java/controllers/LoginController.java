160ef2b5b013d82836105c39d9a7b291137e0bb4
hetay566 <heaty566@gmail.com>
2021-09-18 08:31:50 +0700

    FIX - hashing function

--- src/java/controllers/LoginController.java
+++ src/java/controllers/LoginController.java
@@ -22,89 +22,90 @@ import utils.Helper;
  *
  * @author heaty566
  */
-@WebServlet(name = "LoginController", urlPatterns = { "/LoginController" })
+@WebServlet(name = "LoginController", urlPatterns = {"/LoginController"})
 public class LoginController extends HttpServlet {
 
-	/**
-	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-	 * methods.
-	 *
-	 * @param request  servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException      if an I/O error occurs
-	 */
-	private boolean processRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private boolean processRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		// initialize resource
-		UserDAO userDAO = new UserDAO();
+        // initialize resource
+        UserDAO userDAO = new UserDAO();
 
-		// validate params
-		String username = GetParam.getStringParam(request, "username", "Username", 5, 50, null);
-		String password = GetParam.getStringParam(request, "password", "Password", 5, 50, null);
-		if (username == null || password == null) {
-			return false;
-		}
+        // validate params
+        String username = GetParam.getStringParam(request, "username", "Username", 5, 50, null);
+        String password = GetParam.getStringParam(request, "password", "Password", 5, 50, null);
 
-		// checking exist user and correct password
-		User existedUser = userDAO.getOneUserByUsername(username);
-		int hashingKey = Integer.parseInt(getServletContext().getInitParameter("HashingKey"));
-		if (existedUser == null || !Helper.comparePassword(password, existedUser.getPassword(), hashingKey)) {
-			request.setAttribute("errorMessage", "Username or password is not correct");
-			return false;
-		}
+        if (username == null || password == null) {
+            return false;
+        }
 
-		// handle on login
-		HttpSession session = request.getSession();
-		session.setAttribute("username", existedUser.getUsername());
-		session.setAttribute("role", existedUser.getRole());
-		return true;
-	}
+        // checking exist user and correct password
+        User existedUser = userDAO.getOneUserByUsername(username);
+        int hashingKey = Integer.parseInt(getServletContext().getInitParameter("HashingKey"));
+        if (existedUser == null || !Helper.comparePassword(password, existedUser.getPassword(), hashingKey)) {
+            request.setAttribute("errorMessage", "Username or password is not correct");
+            return false;
+        }
 
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
-	// + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request  servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException      if an I/O error occurs
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		request.getRequestDispatcher(Routers.LOGIN_PAGE).forward(request, response);
-	}
+        // handle on login
+        HttpSession session = request.getSession();
+        session.setAttribute("username", existedUser.getUsername());
+        session.setAttribute("role", existedUser.getRole());
+        return true;
+    }
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 *
-	 * @param request  servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException      if an I/O error occurs
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+    // + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.getRequestDispatcher(Routers.LOGIN_PAGE).forward(request, response);
+    }
 
-		try {
-			if (processRequest(request, response)) {
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
 
-				// forward on 200
-				response.sendRedirect(Routers.INDEX_CONTROLLER);
-				return;
-			}
-			// forward on 400
-			request.getRequestDispatcher(Routers.LOGIN_PAGE).forward(request, response);
+        try {
+            if (processRequest(request, response)) {
 
-		} catch (Exception e) {
-			// redirect on 500
-			request.getRequestDispatcher(Routers.ERROR).forward(request, response);
-		}
-	}
+                // forward on 200
+                response.sendRedirect(Routers.INDEX_CONTROLLER);
+                return;
+            }
+            // forward on 400
+            request.getRequestDispatcher(Routers.LOGIN_PAGE).forward(request, response);
+
+        } catch (Exception e) {
+            // redirect on 500
+            request.getRequestDispatcher(Routers.ERROR).forward(request, response);
+        }
+    }
 
 }
